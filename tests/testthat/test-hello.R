f225c54aeedaba0485f1e8a53cb3929d032da692
whirlsyu <whirlsyu@126.com>
2023-03-13 20:59:32 +0800

    change data

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -4,6 +4,8 @@ test_that("multiplication works", {
   data("demo_data")
   res<-IdentifyMCB(demo_data$realdata)
   expect_equal(as.character(res$MCBinformation[,'location']), 
-   c("chr2 : 74669349 - chr2 : 74669516","chr6 : 108444791 - chr6 : 108444920",
-   "chr8 : 18541446 - chr8 : 18541627","chr8 : 61777711 - chr8 : 61778137"))
+   c("chr2 :  74669349 - chr2 :  74669516",
+     "chr6 : 108444791 - chr6 : 108444920",
+     "chr8 :  18541446 - chr8 :  18541627",
+     "chr8 :  61777711 - chr8 :  61778137"))
 })
